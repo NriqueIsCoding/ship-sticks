062b4a4f1a0b02b8b5a467dc1731f1160b52f485
nrique <eesta83@gmail.com>
2018-09-25 14:56:07 -0400

    bootstrap index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+(function($) {
+	"use strict"
+
+	// Preloader
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
+
+	// Mobile Toggle Btn
+	$('.navbar-toggle').on('click',function(){
+		$('#header').toggleClass('nav-collapse')
+	});
+
+})(jQuery);
